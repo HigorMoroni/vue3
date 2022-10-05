4d00e053d284e492c090fdcab72d89ff7240aa85
Higor Moroni <higor.ferreira@melhorenvio.com>
2022-10-05 17:56:43 -0300

    feat: use ref, computed, method and watch on composition API

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  {{fullName}}
+  <img @click="changeName" alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import { ref } from '@vue/reactivity';
+import { computed, watch } from '@vue/runtime-core';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+  },
+  setup() {
+    //DATA
+    const user = ref({
+      first_name: 'Jon',
+      last_name: 'Snow'
+    });
+    
+    //COMPUTED
+    const fullName = computed(() => `${user.value.first_name} ${user.value.last_name}`);
+
+    //METHOD
+    const changeName = () => {
+      user.value.first_name = 'Sansa';
+    };
+
+    //WATCH
+    watch(user, () => {
+      console.log('alterou algo no objeto');
+    }, {
+      deep: true
+    });
+
+    watch(() => user.value.first_name, () => {
+      console.log('alterou o first name dentro do objeto user')
+    });
+
+    //RETURN
+    return {
+      user,
+      changeName,
+      fullName
+    }
   }
 }
 </script>
