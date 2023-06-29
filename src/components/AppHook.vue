ab90f93e3008ab80da73cb44922a366ae1601f53
Higor Moroni <higor.ferreira@melhorenvio.com>
2023-06-29 18:08:45 -0300

    feat: add functional script setup

--- src/components/AppHook.vue
+++ src/components/AppHook.vue
@@ -2,22 +2,19 @@
   <div>Hook</div>
 </template>
 
-<script>
-import { onBeforeMount, onBeforeUnmount, onMounted, onUnmounted } from '@vue/runtime-core'
-export default {
-  setup() {
-    onBeforeMount(() => {
-      console.log('onBeforeMount');
-    });
-    onMounted(() => {
-      console.log('onMounted');
-    });
-    onBeforeUnmount(() => {
-      console.log('onBeforeUnmount');
-    });
-    onUnmounted(() => {
-      console.log('onUnmounted');
-    });
-  },
-}
+<script setup>
+import { onBeforeMount, onBeforeUnmount, onMounted, onUnmounted } from '@vue/runtime-core';
+
+onBeforeMount(() => {
+  console.log('onBeforeMount');
+});
+onMounted(() => {
+  console.log('onMounted');
+});
+onBeforeUnmount(() => {
+  console.log('onBeforeUnmount');
+});
+onUnmounted(() => {
+  console.log('onUnmounted');
+});
 </script>
